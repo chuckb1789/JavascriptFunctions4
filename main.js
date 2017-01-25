0a31e9e92bc87bfe43546cfd8e3afda54bdffb87
CharlieBeck <chuckb1789@gmail.com>
2017-01-25 08:36:16 -0700

    Complete

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-// var addNumbers = function (str) {
-//   var value = str.split('');
-//   var numbers = []
-//   for (var i=0; i<value.length; i++){
-//     if ((value[i]%2 === 0 || value[i]%2 === 1) && (value[i+1]%2 === 0 || value[i+1]%2 === 1)) {
-//         numbers.push(value[i]+value[i+1]);
-//     } else if((value[i]%2 === 0 || value[i]%2 === 1) && value[i-1]%2 != 0 && value[i-1]%2 != 1) {
-//         numbers.push(value[i]);
-//     }
-//   }
-//   console.log(numbers)
-//   var sum = numbers.reduce(function(a,b){
-//     // return parseInt(a) + parseInt(b);
-//     return +a + +b;
-//   });
-//   return sum
-// }
-//
-// console.log(addNumbers('55Hello'));
-// console.log(addNumbers('5Hello5'));
-// console.log(addNumbers('88Hello 3World'));
+var addNumbers = function (str) {
+  var value = str.split('');
+  var numbers = []
+  for (var i=0; i<value.length; i++){
+    if ((value[i]%2 === 0 || value[i]%2 === 1) && (value[i+1]%2 === 0 || value[i+1]%2 === 1)) {
+        numbers.push(value[i]+value[i+1]);
+    } else if((value[i]%2 === 0 || value[i]%2 === 1) && value[i-1]%2 != 0 && value[i-1]%2 != 1) {
+        numbers.push(value[i]);
+    }
+  }
+  console.log(numbers)
+  var sum = numbers.reduce(function(a,b){
+    // return parseInt(a) + parseInt(b);
+    return +a + +b;
+  });
+  return sum
+}
+
+console.log(addNumbers('55Hello'));
+console.log(addNumbers('5Hello5'));
+console.log(addNumbers('88Hello 3World'));
 
 var longestWord = function (str) {
   var words = str.split(' ');
