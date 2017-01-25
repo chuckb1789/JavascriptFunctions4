229bab0d23ced6627574303c9dfca4b3777bf5b3
CharlieBeck <chuckb1789@gmail.com>
2017-01-25 08:35:16 -0700

    Completed

---
+++ main.js
@@ -0,0 +1,34 @@
+// var addNumbers = function (str) {
+//   var value = str.split('');
+//   var numbers = []
+//   for (var i=0; i<value.length; i++){
+//     if ((value[i]%2 === 0 || value[i]%2 === 1) && (value[i+1]%2 === 0 || value[i+1]%2 === 1)) {
+//         numbers.push(value[i]+value[i+1]);
+//     } else if((value[i]%2 === 0 || value[i]%2 === 1) && value[i-1]%2 != 0 && value[i-1]%2 != 1) {
+//         numbers.push(value[i]);
+//     }
+//   }
+//   console.log(numbers)
+//   var sum = numbers.reduce(function(a,b){
+//     // return parseInt(a) + parseInt(b);
+//     return +a + +b;
+//   });
+//   return sum
+// }
+//
+// console.log(addNumbers('55Hello'));
+// console.log(addNumbers('5Hello5'));
+// console.log(addNumbers('88Hello 3World'));
+
+var longestWord = function (str) {
+  var words = str.split(' ');
+  var longWord = ""
+  for (var i=0; i<words.length; i++) {
+      if (words[i].length>longWord.length) {
+          longWord = words[i]
+      }
+    }
+    return longWord;
+  }
+
+console.log(longestWord('Super Duper Cali Fragilistic'));
